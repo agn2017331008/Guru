add6fcc84e0520c61cc00b4ac1ea11d6f5930be8
agn2017331008 <ghosharpita423@gmail.com>
2023-05-24 18:17:37 +0600

    login

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+User.create(email: 'd@example.com', password: '123456', password_confirmation: 'password')
+
 10.times do |x|
-    Post.create(title: "Title #{x}", body: "Body #{x}")
+    Post.create(title: "Title #{x}", body: "Body #{x}", user_id: User.first.id)
 end    
